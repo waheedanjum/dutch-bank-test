7b88d4207bee31aee3f5be37ad22a3a3dc66f78d
waheedanjum <mwaheedanjum@outlook.com>
2021-07-26 18:12:44 +0100

    work completed

--- ui-test-app/src/app/components/country-details/country-details.component.ts
+++ ui-test-app/src/app/components/country-details/country-details.component.ts
@@ -12,6 +12,7 @@ import {CountriesData} from "../../models/region";
 
 export class CountryDetailsComponent implements OnInit {
   name: string | null = '';
+  selectedCountry: CountriesData[] = [];
 
   countries$: Observable<CountriesData[]> | undefined;
 
@@ -21,15 +22,23 @@ export class CountryDetailsComponent implements OnInit {
    if(value) {
      this.name = JSON.parse(value);
    }
+  }
+
+  ngOnInit(): void {
     if(this.name && this.name.toUpperCase() === 'ASIA') {
-     this.countries$ = this.regionService.getAsianCountries();
+      this.countries$ = this.regionService.getAsianCountries();
     }
     if(this.name && this.name.toUpperCase() === 'EUROPE') {
       this.countries$ = this.regionService.getEuropeanCountries();
     }
   }
 
-  ngOnInit(): void {
+  onCountrySelect(event: any) {
+    let name = event.target.value;
+    this.countries$?.subscribe((data) => {
+      this.selectedCountry = data.filter(x => x.name === name)
+    })
   }
+
 
 }
